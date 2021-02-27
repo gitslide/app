82de40e45a5e70ccfd2acd6cd8f5c2c18b106b0d
tom <tom@sapletta.com>
2021-02-27 11:25:07 +0100

    spolittin to many smaller presentations

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 :: Prepare the developer version without minimizing the content in file jloads.min.js
-type ".\entuzjasta\streaming\*.md" > .\jloads.md
-type ".\entuzjasta\streaming\*.md" > .\jloads.md
-xcopy /y/r jloads.js jloads.min.js
+type ".\tom-sapletta\4developers\*.md" > .\jloads.md
+type ".\tom-sapletta\about\*.md" >> .\jloads.md
+type ".\tom-sapletta\softreck\pl.md" >> .\jloads.md
+type ".\entuzjasta\development\*.md" >> .\jloads.md
+type ".\entuzjasta\internet\*.md" >> .\jloads.md
+type ".\entuzjasta\streaming\*.md" >> .\jloads.md
